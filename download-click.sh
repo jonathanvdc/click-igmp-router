b90d8a7729a180f547c3a5d85943b0d1aa84fe99
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-11 13:57:33 +0200

    Create a script that downloads click

---
+++ download-click.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Download click-2.0.1.
+wget http://www.read.cs.ucla.edu/click/click-2.0.1.tar.gz
+# Unzip the click tar.
+tar xzf click-2.0.1.tar.gz
+# Build click.
+pushd click-2.0.1
+CXXFLAGS="-Wno-narrowing" ./configure --disable-linuxmodule --enable-local --enable-etherswitch
+popd
