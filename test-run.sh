7ede33ba43a83435b28f76787a56b38e31463c00
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-15 20:01:43 +0200

    Include a test run script and use it for CI builds

---
+++ test-run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+timeout 240 ./click-2.0.1/userlevel/click -p 10000 scripts/ipnetwork.click &
+sleep 5
+./join.sh client31
+./join.sh client22
+sleep 5
+./join.sh client21
+./join.sh client32
+sleep 5
+# Should not do anything.
+./join.sh client31
+./join.sh client31
+sleep 5
+# Make sure that leave/join in rapid succession actually work.
+./leave.sh client21
+./join.sh client21
+./leave.sh client21
+sleep 5
+# Have all clients except client32 leave the group.
+./leave.sh client21
+./leave.sh client22
+./leave.sh client31
+sleep 5
+# Now wait to make sure that the router keeps on forwarding messages to client32.
+wait
