09d8c38e50c13e7a5c93944f55bd61b345d0cd51
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-15 18:06:00 +0200

    Format IgmpRouter.hh

--- elements/IgmpRouter.hh
+++ elements/IgmpRouter.hh
@@ -13,98 +13,98 @@ class IgmpRouter;
 
 class IgmpRouter : public Element
 {
-public:
-  IgmpRouter();
-  ~IgmpRouter();
+  public:
+    IgmpRouter();
+    ~IgmpRouter();
 
-  // Description of ports:
-  //
-  //     Input:
-  //         0. Incoming IP packets which are filtered based on their source
-  //            address.
-  //
-  //         1. Incoming IGMP packets.
-  //
-  //     Output:
-  //         0. Generated IGMP packets.
-  //
-  //         1. Incoming IP packets which have been filtered based on their
-  //            source address.
-  //
-  //         2. Incoming IP packets which were filtered out. The router does
-  //            not believe that these are multicast packets intended for a
-  //            client on the network.
+    // Description of ports:
+    //
+    //     Input:
+    //         0. Incoming IP packets which are filtered based on their source
+    //            address.
+    //
+    //         1. Incoming IGMP packets.
+    //
+    //     Output:
+    //         0. Generated IGMP packets.
+    //
+    //         1. Incoming IP packets which have been filtered based on their
+    //            source address.
+    //
+    //         2. Incoming IP packets which were filtered out. The router does
+    //            not believe that these are multicast packets intended for a
+    //            client on the network.
 
-  const char *class_name() const { return "IgmpRouter"; }
-  const char *port_count() const { return "2/3"; }
-  const char *processing() const { return PUSH; }
+    const char *class_name() const { return "IgmpRouter"; }
+    const char *port_count() const { return "2/3"; }
+    const char *processing() const { return PUSH; }
 
-  int configure(Vector<String> &, ErrorHandler *);
+    int configure(Vector<String> &, ErrorHandler *);
 
-  void push(int port, Packet *packet);
+    void push(int port, Packet *packet);
 
-private:
-  /// A timer callback that sends periodic general queries.
-  struct SendPeriodicGeneralQuery
-  {
-    SendPeriodicGeneralQuery()
-        : elem(nullptr)
+  private:
+    /// A timer callback that sends periodic general queries.
+    struct SendPeriodicGeneralQuery
     {
-    }
-    SendPeriodicGeneralQuery(IgmpRouter *elem)
-        : elem(elem)
+        SendPeriodicGeneralQuery()
+            : elem(nullptr)
+        {
+        }
+        SendPeriodicGeneralQuery(IgmpRouter *elem)
+            : elem(elem)
+        {
+        }
+        IgmpRouter *elem;
+
+        void operator()() const;
+    };
+
+    /// A timer callback that sends a group-specific query.
+    struct SendGroupSpecificQuery
     {
-    }
-    IgmpRouter *elem;
+        SendGroupSpecificQuery()
+            : elem(nullptr), group_address()
+        {
+        }
+        SendGroupSpecificQuery(IgmpRouter *elem, const IPAddress &group_address)
+            : elem(elem), group_address(group_address)
+        {
+        }
+        IgmpRouter *elem;
+        IPAddress group_address;
 
-    void operator()() const;
-  };
+        void operator()() const;
+    };
 
-  /// A timer callback that sends a group-specific query.
-  struct SendGroupSpecificQuery
-  {
-    SendGroupSpecificQuery()
-        : elem(nullptr), group_address()
+    /// A timer callback for the other querier present timer.
+    struct OtherQuerierGone
     {
-    }
-    SendGroupSpecificQuery(IgmpRouter *elem, const IPAddress &group_address)
-        : elem(elem), group_address(group_address)
-    {
-    }
-    IgmpRouter *elem;
-    IPAddress group_address;
+        OtherQuerierGone()
+            : elem(nullptr)
+        {
+        }
+        OtherQuerierGone(IgmpRouter *elem)
+            : elem(elem)
+        {
+        }
+        IgmpRouter *elem;
 
-    void operator()() const;
-  };
+        void operator()() const;
+    };
 
-  /// A timer callback for the other querier present timer.
-  struct OtherQuerierGone
-  {
-    OtherQuerierGone()
-        : elem(nullptr)
-    {
-    }
-    OtherQuerierGone(IgmpRouter *elem)
-        : elem(elem)
-    {
-    }
-    IgmpRouter *elem;
+    void handle_igmp_packet(Packet *packet);
+    void handle_igmp_membership_query(const IgmpMembershipQuery &query, const IPAddress &source_address);
+    void transmit_membership_query(const IgmpMembershipQuery &query);
+    void init_startup_queries();
 
-    void operator()() const;
-  };
-
-  void handle_igmp_packet(Packet *packet);
-  void handle_igmp_membership_query(const IgmpMembershipQuery &query, const IPAddress &source_address);
-  void transmit_membership_query(const IgmpMembershipQuery &query);
-  void init_startup_queries();
-
-  IPAddress address;
-  IgmpRouterFilter filter;
-  EventSchedule<SendGroupSpecificQuery> query_schedule;
-  CallbackTimer<SendPeriodicGeneralQuery> general_query_timer;
-  unsigned int startup_general_queries_remaining;
-  bool other_querier_present = false;
-  CallbackTimer<OtherQuerierGone> other_querier_present_timer;
+    IPAddress address;
+    IgmpRouterFilter filter;
+    EventSchedule<SendGroupSpecificQuery> query_schedule;
+    CallbackTimer<SendPeriodicGeneralQuery> general_query_timer;
+    unsigned int startup_general_queries_remaining;
+    bool other_querier_present = false;
+    CallbackTimer<OtherQuerierGone> other_querier_present_timer;
 };
 
 CLICK_ENDDECLS
